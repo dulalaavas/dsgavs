d0203606f278a2dbeff104a501c5a5f7959c0939
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-13 15:53:42 +0545

    assignmn

--- tasks/assignment1.c
+++ tasks/assignment1.c
@@ -63,14 +63,13 @@ int main(){
 //     printf("The perimeter of the rectangle is: %1.3f \n", perimeter);
 
 // calculate compound interest
-    float principle2, rate2, time2, compoundInterest;
-    printf("Enter principle amount:");
-    scanf("%f", &principle2);
-    printf("Enter rate of interest:");
-    scanf("%f", &rate2);
-    printf("Enter number of years:");
-    scanf("%f", &time2);
-    compoundInterest = principle2 * pow((1 + rate2 / 100), time2)- principle2;
-    printf("The compound interest is: %1.3f \n", compoundInterest);
-
+    // float principle2, rate2, time2, compoundInterest;
+    // printf("Enter principle amount:");
+    // scanf("%f", &principle2);
+    // printf("Enter rate of interest:");
+    // scanf("%f", &rate2);
+    // printf("Enter number of years:");
+    // scanf("%f", &time2);
+    // compoundInterest = principle2 * pow((1 + rate2 / 100), time2)- principle2;
+    // printf("The compound interest is: %1.3f \n", compoundInterest);
 }
