a4a450db20766f1cb0ea850be5d10ec30fa67bfa
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-13 16:30:03 +0545

    assignmn

--- tasks/assignment2.c
+++ tasks/assignment2.c
@@ -14,7 +14,7 @@ int main(){
         // char letter;
         // printf("Enter a character: ");
         // scanf("%c", &letter);
-        // if (letter == 'a','e','i','o','u'){
+        // if (letter == 'a' || letter ==  'e'|| letter == 'i' || letter == 'o' || letter == 'u'){
         //     printf("%c is a vowel. \n", letter);
         // }else{
         //     printf("%c is not a vowel. \n", letter);
