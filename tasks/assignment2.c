b57f682031b4bb91deb92d8fc5d919086840f154
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-13 16:20:59 +0545

    assignmn

---
+++ tasks/assignment2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main(){
+    //eligibility to vote
+        // int age;
+        // printf("Enter your age: ");
+        // scanf("%d", &age);
+        // if (age >= 18){
+        //     printf("You are eligible to vote. \n");
+        // }else{
+        //     printf("You are not eligible to vote. \n");
+        // }
+        
+    //check if a character is vowel or not
+        // char letter;
+        // printf("Enter a character: ");
+        // scanf("%c", &letter);
+        // if (letter == 'a','e','i','o','u'){
+        //     printf("%c is a vowel. \n", letter);
+        // }else{
+        //     printf("%c is not a vowel. \n", letter);
+        // }
+
+    //checkk if the year is a leap year or not
+        // int year;
+        // printf("Enter any year: ");
+        // scanf("%d", &year);
+        // if (year % 4 == 0){
+        //     printf("%d is a leap year. \n", year);
+        // }else{
+        //     printf("%d is not a leap year. \n", year);
+        // }
+
+    //to classify a person’s weight status based on their BMI
+        // float weight, height, bmi;
+        // printf("Enter your weight and height :");
+        // scanf("%f %f", &weight, &height);
+        // bmi = weight / (height * height);
+        // if (bmi < 18.5){
+        //     printf("Your BMI is %f, you are underweight.", bmi);
+        // }else if (bmi >= 18.5 && bmi < 25){
+        //     printf("Your BMI is %f, you are normal weight.", bmi);
+        // }else if (bmi >= 25 && bmi < 30){
+        //     printf("Your BMI is %f, you are overweight.", bmi);
+        // }else{
+        //     printf("Your BMI is %f, you are obese.", bmi);
+        // }
+
+}
