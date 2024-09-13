8bdced1a5a00f2fee36f25024bea67c4fe607da8
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-13 16:30:52 +0545

    assignmn

--- tasks/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-char letter;
-        printf("Enter a character: ");
-        scanf("%c", &letter);
-        if (letter == 'a','e','i','o','u'){
-            printf("%c is a vowel. \n", letter);
-        }else{
-            printf("%c is not a vowel. \n", letter);
-        }
