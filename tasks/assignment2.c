5a56e5d600d5add02968a0d7b61fae96407af33a
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-14 19:41:16 +0545

    assignmn

--- tasks/assignment2.c
+++ tasks/assignment2.c
@@ -124,5 +124,18 @@ int main(){
         //     printf("The oldest person is %d years old. \n", age3);
         // }
 
-    // 
+    //calculate electricity bill
+        // int units;
+        // float bill;
+        // printf("Enter the number of units consumed: ");
+        // scanf("%d", &units);
+        // if (units <= 100){
+        //     bill = units * 5;
+        // }else if (units <= 200){
+        //     bill = 100 * 5 + (units - 100) * 10;
+        // }else{
+        //     bill = 100 * 5 + 100 * 10 + (units - 200) * 15;
+        // }
+        // printf("Your electricity bill is: %.2f \n", bill);
+
 }
