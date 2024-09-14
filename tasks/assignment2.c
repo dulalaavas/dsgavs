478e0c7bec5cf178804ec3edc3d253d3db8e06a0
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-14 19:36:46 +0545

    assignmn

--- tasks/assignment2.c
+++ tasks/assignment2.c
@@ -112,4 +112,17 @@ int main(){
         //     printf("%d is zero. \n", number);
         // }
 
+    //to determine the oldest among three people based on their ages
+        // int age1, age2, age3;
+        // printf("Enter the ages of three people: ");
+        // scanf("%d %d %d", &age1, &age2, &age3);
+        // if (age1 > age2 && age1 > age3){
+        //     printf("The oldest person is %d years old. \n", age1);
+        // }else if (age2 > age3){
+        //     printf("The oldest person is %d years old. \n", age2);
+        // }else{
+        //     printf("The oldest person is %d years old. \n", age3);
+        // }
+
+    // 
 }
