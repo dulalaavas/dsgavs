72cb91cb6d3b6c8fc1072d4d7dd43be28fd95411
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-18 16:09:14 +0545

    assignmn

---
+++ tasks/Classwork1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    //Print 5x5 * pattern
+        // int i,j;
+        // for( i=1;i<=5;i++){
+        //     for(j=1;j<=5;j++){
+        //         printf("*");
+        //     }
+        //     printf("\n");
+        // }
+
+    //Print right angled triangle of * lefthand
+        // int a,b;
+        // for(a=1;a<=5;a++){
+        //     for(b=1;b<=a;b++){
+        //          printf("*");
+        //      }
+        //      printf("\n");
+        // }
+
+    //Print right angle triangle of * righthand
+        // int c,d;
+        // for(c=1;c<=5;c++){
+        //     for(d=1;d<=5;d++){
+        //         if ((c+d)<=5){
+        //             printf(" ");
+        //         }else{
+        //             printf("*");
+        //         }
+        // }
+        // printf("\n");
+        // }
+}
