d8f7f340bb88170633d592906de1b132065367d1
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-18 15:29:33 +0545

    assignmn

--- Classroom/syntax.c
+++ Classroom/syntax.c
@@ -1,11 +1,24 @@
-#include <stdio.h>
-int main(){
+#include<stdio.h>
 
-    // do while loop
-    int i = 1;
-    do{
-        printf("%d ", i);
-         i++;
-    }while(i <= 10);
-    printf("\n");
+int main(){
+  
+  int pin = 1111;
+  int attempts = 0;
+  int input;
+  
+  retry:
+  printf("Enter pin:");
+  scanf("%d",&input);
+  
+  if(pin == input){
+      printf("Access Granted");
+  }else{
+      attempts++;
+      printf("Invalid pin, Attempt %d of 3\n",attempts);
+      if(attempts<3){
+          goto retry;
+      }else{
+          printf("Too many attempts, try again later");
+      }
+  }
 }
