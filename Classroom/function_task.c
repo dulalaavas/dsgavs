f9db81d5b523407f00a6a49f2359272e57765a35
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-20 16:34:11 +0545

    classwork

--- Classroom/function_task.c
+++ Classroom/function_task.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+int add(int, int);
+void sub(int, int);    
+int mul(int, int);
+
 int main(){
     int first,second,choice;
     printf("Chose option(1-3) \n");
@@ -13,7 +17,33 @@ int main(){
         printf("Enter second number: ");
         scanf("%d",&second);
 
+        switch (choice){
+        case 1:{
+            int sum = add(first, second);
+            printf("The sum is %d", sum );
+            break;}
+        case 2:{
+            sub(first, second);
+            break;}
+        case 3:{
+            int product = mul(first, second);
+            printf("The product is %d", product);
+            break;}
+        default:
+            break;
+        }
+
     }else{
         printf("Invalid choice \n");
     }
+}
+
+int add(int a, int b){
+    return a+b;
+}
+void sub(int a, int b){
+    printf("Sub is %d", a-b);
+}
+int mul(int a, int b){
+    return a*b;
 }
