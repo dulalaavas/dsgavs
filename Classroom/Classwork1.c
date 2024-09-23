2d64ffe6ed592aa1f61782fc14110ca5b1765e7a
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-23 16:22:02 +0545

    Classwork

--- Classroom/Classwork1.c
+++ Classroom/Classwork1.c
@@ -11,19 +11,34 @@
 //         printf("Your new salary is : %d",*ptr1 * 2);
 // }
 
-int calculate(int *a);
+// int calculate(int *a);
 
-int main() {
-   int salary;
-   int *ptr = &salary;
-   printf("Enter salary: ");
-   scanf("%d",ptr);
-   printf("Your original salary is %d\n",*ptr);
+// int main() {
+//    int salary;
+//    int *ptr = &salary;
+//    printf("Enter salary: ");
+//    scanf("%d",ptr);
+//    printf("Your original salary is %d\n",*ptr);
    
-   int new = calculate(ptr);
-   printf("Your new salary is %d",new);
+//    int new = calculate(ptr);
+//    printf("Your new salary is %d",new);
+// }
+
+// int calculate(int *a){
+//     return *a * 2;
+// }
+
+int mul(int);
+int main(){
+    int num = 5;
+    int c = mul(num);
+    printf("The product of the numbers from 1 to %d is %d", num, c);
 }
 
-int calculate(int *a){
-    return *a * 2;
+int mul(int a){
+    if(a == 0){
+        return 1;
+    } else {
+        return a * mul(a-1);
+    }
 }
