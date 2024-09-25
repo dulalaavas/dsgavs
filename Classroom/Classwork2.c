15c9599c0a397c1e377b7a4d9887316107e6ad8d
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-09-25 15:38:11 +0545

    Classwork

--- Classroom/Classwork2.c
+++ Classroom/Classwork2.c
@@ -1,21 +1,19 @@
 #include<stdio.h>
-int swap(int *a, int *b){
-
-}
+void swap(int *a, int *b);
 int main(){
-    int *a = 10;
-    int *b = 20;
-
-    printf("The value of a is %d \n", a);
-    printf("The value of b is %d \n", b);
-
-    int c = a;
-    a = b;
-    b = c;
-
-    printf("The new value of a is %d \n", a);
-    printf("The new value of b is %d \n", b);
+    int a = 10;
+    int b = 20;
+    int *c = &a;
+    int *d = &b;
+    printf("Value of a is: %d \n", a);
+    printf("Value of b is: %d \n", b);
+    swap(c,d);
+    printf("New value of a is: %d \n", a);
+    printf("New value of b is: %d \n", b);
 }
-int swap(int *a, int *b){
-    
+void swap(int *a, int *b){
+    int x;
+    x = *a;
+    *a = *b;
+    *b = x;
 }
